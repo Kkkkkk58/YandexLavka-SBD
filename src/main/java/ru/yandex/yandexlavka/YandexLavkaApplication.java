60e70ed742e0fe631ab480c752868f93b44a5332
Karim Khasan <karimhasan3879@gmail.com>
2023-05-14 22:47:16 +0300

    refactor: remove extra configuration annotations

--- src/main/java/ru/yandex/yandexlavka/YandexLavkaApplication.java
+++ src/main/java/ru/yandex/yandexlavka/YandexLavkaApplication.java
@@ -4,13 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "ru.yandex.yandexlavka")
+@SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Yandex Lavka", version = "1.0"))
-@EnableJpaRepositories(value = "ru.yandex.yandexlavka.dataaccess.repositories")
-@EntityScan("ru.yandex.yandexlavka.dataaccess.entities")
 public class YandexLavkaApplication {
 
     public static void main(String[] args) {
